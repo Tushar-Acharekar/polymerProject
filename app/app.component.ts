d3e22b74cb7aa60d71229e7546438d080d49223d
acharekart <acharekart@gmail.com>
2017-09-27 04:40:30 -0400

    new_angular_binding_10

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
-import {Component, DynamicComponentLoader, ElementRef, ComponentRef, Injector, ViewChild} from 'angular2/core';
+import {Component, DynamicComponentLoader, ElementRef, ComponentRef, Injector, ViewChild, NgModule, CUSTOM_ELEMENTS_SCHEMA,} from 'angular2/core';
 import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators} from 'angular2/common';
-
+import { PolymerModule, PolymerElement } from '@vaadin/angular2-polymer';
 
 
 @Component ({
@@ -9,6 +9,7 @@ import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators} from 'angular2/c
    <div>
       <h1>{{appTitle}}</h1>
       <div>To Tutorials Point</div>
+			<paper-input [(value)]="appTitle"></paper-input>
    </div>
 	`  
 })
